e0f75e099a942430b379fbb8ef630b1794778c17
박재한 <lighthouse97@naver.com>
2021-04-09 11:26:17 +0900

    BOJ 2458 more optimization is done!!!

--- BaekJoon/2458.cpp
+++ BaekJoon/2458.cpp
@@ -7,8 +7,12 @@
 // 예로 문제에서 정점 4는 앞뒤로 모든 정점이 연결되어 있고, 나머지 정점은 앞뒤로 연결이 전혀 안된 것이 하나 이상 있다.
 // 그게 답이다!
 // dfs를 이용해서 풀면 좀 더 빨리 처리가 된다.
+// floyd_warshall() 함수에서 g_graph[i][j], g_grapj[i][k], g_graph[k][j] 모두 query하는 방법과 g_grapj[i][k], g_graph[k][j] 2개만 query하는 방법
+// 이 있는데, g_grapj[i][k], g_graph[k][j] 2개만 query하는 방법이 훨씬 수행시간이 줄어든다.
+// 대략, 340ms -> 132ms로 수행시간이 1/3 가량 줄어든다.
 // 제한시간 1초 중 340ms(3,080KB)가 소요되었다.
 // 맞은사람 1801/2824로 상위 63.77%에 rank되었다.
+// floyd_warshall() 함수에서 최적화 했을 경우, 340ms -> 132ms로 최적화되고 맞은 사람 1027/2825로 상위 36.35%에 rank 된다. 
 
 #include "pch.h"
 //#include <cstdio> // NULL
@@ -42,8 +46,9 @@ void floyd_warshall()
 	for (int k = 1; k < g_N + 1; k++) {
 		for (int i = 1; i < g_N + 1; i++) {
 			for (int j = 1; j < g_N + 1; j++) {
-				if (!g_graph[i][j]) // 갈 수 없는 경우만 따진다.
-					g_graph[i][j] = g_graph[i][k] * g_graph[k][j];
+				//if (!g_graph[i][j]) // 갈 수 없는 경우만 따진다.
+				//	g_graph[i][j] = g_graph[i][k] * g_graph[k][j];
+				if (g_graph[i][k] & g_graph[k][j]) g_graph[i][j] = 1;
 			}
 		}
 	}
