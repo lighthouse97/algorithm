8750e33660390d03ff5eb4f55ea390b765e7e292
박재한 <lighthouse97@naver.com>
2021-06-30 14:52:29 +0900

    BOJ 17412, Network Flow, Edmond-Karp, Unifying the implementation pattern.

--- BaekJoon/17412.cpp
+++ BaekJoon/17412.cpp
@@ -1,76 +1,97 @@
-// 17412.cpp
-// 도시 왕복하기1
-// 가장 기본적인 Network Flow문제이다.
-// 즉 주어진 그래프에서 출발점에서 목적지까지 maximum flow, 즉 가장 큰 유량을 구하는 문제이다.
-// 이 문제를 풀기위한 대표적인 알고리즘들이 있는데,
-// 그 중에서 BFS를 활용한 Edmonds-Karp algorithm을 사용하여 구현하였다.이는 Ford-Fulkerson algorithm을 기본으로 BFS로 변형한 것이다.
-// 자 함 풀어보자!
-// 주의할 점!
-// 단방향이라 역방향 간선이나 역방향의 capacity는 없어야 한다.(양방향 문제는 따로 있다. 없어야 0이 된다. 괜히 만들면 틀린다.)
-// 알고리즘 특성상 역방향에 대한 flow 값은 항상 존재해야 제대로 값을 찾아갈 수 있다.
-// network flow를 잘 이해하지 못한 상태에서 어설프게 문제를 푸니 쓸데없이 시간만 많이 잡아먹었다!!!
-// 제한시간 2초 중 4ms(3,308KB)가 소요되었다.
-// 맞은사람 60/206로 상위 29.12%에 rank되었다.
+// 17412_1.cpp
+// 도시 왕복하기 1
+// Network Flow, Edmond-Karp algorithm
+// 정점사이의 간선이 양방향 또는 여러 개인 경우의 문제들이 있다.
+// 이런 경우는 기존 방식의 Edmond-Karp 알고리즘 코드로는 풀기가 애매하기 때문에
+// 처음부터 이를 고려한 코드로 다시 구현하였다.
+// 즉 g_graph에 간선 정보 및 flow, capacity 모두 다 때려넣고 구헌하였다.
+// 이 방식이 기존 단방향 문제도 모두 포함하기 때문에 이 코드를 표준 Edmind-Karp 코드로
+// 사용하기로 한다.
+// 8ms(2,552KB) 소요되었다.
+// 기존 코드 4ms(3,308KB)에 비해 수행시간은 약간 손해를 보지만 코드 사이즈는 줄어 들었다.
+// 이렇게 구현을 통일시켜서 소스를 관리하도록 한다.(약간의 수행시간 손해는 보지만 크게 개의치 않겠다!)
 
 #include "pch.h"
 //#include <cstdio> // NULL
 //#include <cstdlib> // abs
 #include <iostream>
+//#include <memory> // shared_ptr<>
+//#include <iomanip>
 //#include <cstring> // memset
-//#include <cmath> // pow, sqrt, fabs, hypot, round
+//#include <cmath> // pow, sqrt, fabs. ceil, hypot
 #include <vector>
-#include <algorithm> // sort, max, min, fill
-//#include <climits> // INT_MAX
+//#include <list>
+#include <algorithm> // sort, max, min, copy, swap, unique
+//#include <climits> // INT_MAX, LLONG_MIN
 //#include <string> // string, stoi
-//#include <utility> // pair, swap
+//#include <utility> // pair
 //#include <stack>
 //#include <set>
 #include <queue> // queue, priority_queue
+//#include <deque>
+//#include <functional> // greater
+//#include <map>
+//#include <unordered_map> // unordered_map
 #define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
 using namespace std;
 
-struct vertex {
-	int capacity;
-	int flow;
+const int MAXVAL = 10000;
+
+struct edge {
+	int to, flow, capa, revi;
+	edge() : to(-1), flow(0), capa(0), revi(-1) {}
+	edge(int t, int f, int c, int r) : to(t), flow(f), capa(c), revi(r) {}
+	bool space() { return capa > flow; }
 };
 
-vector<vector<vertex>> g_flow; // flow/capacity information
-vector<int> g_prev;
-int g_S = 0, g_T = 0, g_N = 0; // start, sink(terminal), number of vertex
-int g_maxFlow = 0; // 최대 유량
+vector<vector<edge>> g_graph;
+int g_N, g_S, g_T; // source, sink, n count
+int g_maxFlow = 0;
+
+void addEdge(int from, int to, int flow, int capa)
+{
+	g_graph[from].push_back({ to, flow, capa, (int)g_graph[to].size() });
+	g_graph[to].push_back({ from, 0, 0, (int)g_graph[from].size() - 1 });
+}
 
 void edmond_karp()
 {
-	int current;
-	int maxval = 10000;
-	queue<int> myq;
+	struct vt {
+		int num, index;
+		vt(int n, int i) : num(n), index(i) {}
+	};
+	vector<vt> prev;
 	while (true) {
-		//g_prev = vector<int>(g_prev.size(), -1);
-		fill(g_prev.begin(), g_prev.end(), -1);		
-		myq.push(g_S);
-		while (!myq.empty() && g_prev[g_T] == -1) {
-			current = myq.front();
-			myq.pop();			
-		   for(int next = 1; next < g_N + 1; next++) {				
-				if (g_prev[next] != -1) continue; // 이게 현재 탐색에서 이미 갔던 곳으로 가는 것을 막아주는 역할을 한다.
-				                                  // 다음 번 탐색에서는 다시 초기화 된다.
-				if (g_flow[current][next].capacity - g_flow[current][next].flow > 0) { // 더 흘릴 수 있는 flow가 남은 간선을 찾는다.
-					g_prev[next] = current;
-					myq.push(next);					
+		prev.assign(g_N + 1, { -1, -1 }); // 시작하기 전에 prev를 모두 -1로 초기화시킨다.
+		queue<int> myq;
+		myq.push(g_S); // source -> queue
+		prev[g_S].num = g_S;
+		int cur;
+		while (!myq.empty()) {
+			cur = myq.front();
+			myq.pop();
+			for (int i = 0; i < g_graph[cur].size(); i++) {
+				edge& next = g_graph[cur][i];
+				if (prev[next.to].num == -1 && next.space()) { // 이전에 방문하지 않아야 하고, 더 흘릴 flow가 있을 경우, 다음 정점으로 진행.
+					prev[next.to].num = cur;
+					prev[next.to].index = i;
+					myq.push(next.to);
 				}
 			}
+			if (prev[g_T].num != -1) break; // source -> sink까지 경로 찾았음!
 		}
-		while (!myq.empty()) myq.pop(); // queue clear
-		// flow update!
-		if (g_prev[g_T] == -1) break; // '출발 -> 목적지'까지 모든 경로 탐색하고 더 탐색할 필요 없을 경우이다!
-		int minFlow = maxval;
-		for (int i = g_T; i != g_S; i = g_prev[i]) { // sink -> source까지 역으로 거슬러 온다.
-			minFlow = min(minFlow, g_flow[g_prev[i]][i].capacity - g_flow[g_prev[i]][i].flow);
+		if (prev[g_T].num == -1) break; // source -> sink 까지 흐름이 없을 경우 flow를 다 찾은 경우이므로 더 찾을 필요 없다.
+
+		int minFlow = MAXVAL;
+		for (int i = g_T; i != g_S; i = prev[i].num) { // 잔여(residual)유량이 가장 작은 것으로만 통과 가능하다
+			edge& link = g_graph[prev[i].num][prev[i].index];
+			minFlow = min(minFlow, link.capa - link.flow);
 		}
-		for (int i = g_T; i != g_S; i = g_prev[i]) {
-			g_flow[g_prev[i]][i].flow += minFlow;
-			g_flow[i][g_prev[i]].flow -= minFlow; // 알고리즘 특성 상 역방향 flow 고려
+		for (int i = g_T; i != g_S; i = prev[i].num) {
+			edge& link = g_graph[prev[i].num][prev[i].index];
+			link.flow += minFlow; // 정방향은 flow 더해주고
+			g_graph[link.to][link.revi].flow -= minFlow; // 역방향은 flow 빼준다
 		}
 		g_maxFlow += minFlow;
 	}
@@ -78,21 +99,15 @@ void edmond_karp()
 
 int main()
 {
-	FAST;
-
-	int n, p;
-	cin >> n >> p;
-	g_N = n;
-	g_flow.resize(n + 1, vector<vertex>(n + 1, { 0, 0 }));
-	g_prev.resize(n + 1, -1);
-
-	g_S = 1;
-	g_T = 2;
-	int from, to;
-	for (int i = 0; i < p; i++) {
-		cin >> from >> to;				
-		g_flow[from][to].capacity++; // 간선 중복방문이 안되기 때문에 간선의 capacity는 1로 둔다		
+	int N, P;
+	cin >> N >> P;
+	g_S = 1; g_T = 2; g_N = N;
+	g_graph.resize(g_N + 1);
+	int a, b;
+	for (int i = 1; i < P + 1; i++) {
+		cin >> a >> b;
+		addEdge(a, b, 0, 1);
 	}
-	edmond_karp(); // edmonds-karp algorithm
+	edmond_karp();
 	cout << g_maxFlow << "\n";
 }
